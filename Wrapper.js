c004450c327165181d544d5e3a047a0a289bf88c
lyj0047 <lyj0047@naver.com>
2022-05-11 20:12:56 +0900

    lastplzlast

---
+++ Wrapper.js
@@ -0,0 +1,13 @@
+function Wrapper(props) {
+  const style = {
+    border: '2px solid black',
+    padding: '16px',
+  };
+  return (
+    <div style={style}>
+			{props.children}
+    </div>
+  )
+}
+
+export default Wrapper;
